5ac5dcc0e4c3e8479309c92db8d761b32cae4a42
Ra.One <Ra.One@DESKTOP-F5BRIJ9>
2022-11-01 19:41:04 +0530

    Adding new class

---
+++ JenkinsGithub/src/test/java/com/tests/Practice2.java
@@ -0,0 +1,18 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+
+	@Test
+	public void test01()
+	{
+		System.out.println("hello India");
+	}
+	
+	@Test
+	public void test02()
+	{
+		System.out.println("Jai Maharashtra");
+	}
+}
